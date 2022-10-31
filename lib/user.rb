d8aadbacee2f3a1b86ff35e09dbf8c88dcd358cd
Juspher Mokua <juspermokua6@gmail.com>
2022-11-01 00:46:25 +0300

    done

---
+++ lib/user.rb
@@ -0,0 +1,16 @@
+class User
+
+    def first_name=(fname)
+        @first_name = fname
+    end
+
+    def first_name
+        @first_name
+    end
+    def last_name=(lname)
+        @last_name = lname
+    end
+    def last_name
+        @last_name
+    end
+end
